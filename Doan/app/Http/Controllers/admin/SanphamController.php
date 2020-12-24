ce5d51d9674b12801cb0c2a15b92871f994d8b84
minhchien <chientrieu@gmail.com>
2020-12-24 12:53:49 +0700

    them code

--- Doan/app/Http/Controllers/admin/SanphamController.php
+++ Doan/app/Http/Controllers/admin/SanphamController.php
@@ -21,5 +21,40 @@ class SanphamController extends Controller
             'hangsx' => $hangsx,
         ]);
     }
-
+    public function themsp(Request $request){
+        
+        $request->validate([
+            'hinhanh'=>'required',
+            'hinhanh.*'=>'image|mimes:jpeg,jpg,png,gif',
+        ],[
+            'hinhanh.*.image'=>"Không phải file hình",
+            'hinhanh.*.mimes'=>"file hình là: jpeg,jpg,png,gif"
+        ]);
+       
+        $idsanpham = DB::table('sanpham')->insertGetId([
+            'tensp' => $request->tensanpham,
+            'soluong' => $request->soluong,
+            'dongia' => $request->giatien,
+            'giakhuyenmai'=>$request->giakhuyenmai,
+            'RAM' =>$request->RAM,
+            'CPU' =>$request->CPU,
+            'VGA' => $request->VGA,
+            'manhinh' =>$request->manhinh,
+            'hedieuhanh' =>$request->hedienhanh,
+            'HANGSXma_nhasanxuat'=>$request->nhasanxuat
+        ]);
+        foreach ($request->hinhanh as $value) {
+            $extension = $value -> getClientOriginalExtension();
+                $name = Str::random(15).'.'.$extension;
+                $path = 'images/product';
+                $value->move($path,$name);
+                DB::table('hinh')->insert([
+                'url'=>$name,
+                'tenhinh'=>'sanpham',
+                'SANPHAMma_sp'=>$idsanpham
+                ]);
+            }
+        return redirect(route('homeql'));  
+    }
+    
 }
